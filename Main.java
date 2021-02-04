a5b34f3b0f712dc3b4dc9c2c3c09f94eb104b718
MayureshS7 <mayuresh.suryawanshi@patelhs.org>
2021-02-04 14:16:46 +0000

    Done with the project.

--- Main.java
+++ Main.java
@@ -10,32 +10,39 @@ public static void main(String[] args)
 		
 		System.out.println("\nHow many chicken sandwiches do you want?");
 		int chickenSandwiches = scan.nextInt();
-		
+		//Print statement for name
 		System.out.println("What is your name?");
 		String name = scan.next();
-
+		//Print statement for people
 		System.out.println("\nHow many people are with you?");
 		int amountPeople = scan.nextInt();
 
-		System.out.println("\nThank you " + name + " you have " + amountPeople + " with you.");
-
-		int price = priceTotal(chickenSandwiches, amountPeople);
+		System.out.println("\nThank you " + name + " you have " + amountPeople + " people with you.");
+		
+		int price = priceTotal(chickenSandwiches);
 		System.out.println("\nYour total is $" + price);
 
-		System.out.println(goodBye(name));
+		//parameter for the giftcard
+		System.out.println("Your giftcard amount = " + giftCard(price));
+
 	}	
 
 	//priceTotal method calculates price per person.
-	static int priceTotal(int chickenSandwiches, int amountPeople)
+	static int priceTotal(int chickenSandwiches)
 	{
-		int price = chickenSandwiches + amountPeople;
+		int price = chickenSandwiches * 5;
 		return price;
 	}
 
-	//Good bye method.
-	static String goodBye(String name)
+	//Giftcard method
+	static int giftCard(int price)
 	{
-		String cya = "\nThank you for coming to KFC :) " + name;
-		return cya;
+		if (price >= 50){
+			System.out.println("\nYou will get a 5$ KFC Giftcard congratulations!");
+			return 5;
+		}else{
+			System.out.println("Sorry you will not get a Giftcard.");
+			return 0;
+		}
 	}
 }
